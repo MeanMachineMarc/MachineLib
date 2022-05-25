078c1ae654100b903db47a7e192adfe4e3560c4d
MeanMachineMarc <marcus9981@icloud.com>
2022-05-25 17:07:10 +0100

    Added bin_P

--- MachineLib.py
+++ MachineLib.py
@@ -115,3 +115,31 @@ def factorial(input):
 
 def nCr(n,r):
     return (factorial(n)) / (factorial(int(n) - int(r)) * factorial(r))
+
+#Let X be the discrete random variable 'number of times event occurs.'
+# X ~ B(n, p), where n is the number of trials, and p is the probability of an event occuring during a trial.
+def bin_P(tail, r, n, p):
+    r = int(r)
+    n = int(n)
+    p = float(p)
+
+    if tail == 'L': #P(X <= r)
+       total_probability = (1 - p) ** n 
+       for r in range (1, (r + 1)): #Sum of probabilities up to and including r
+           total_probability = total_probability + (nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
+        
+       return round_xsf(total_probability, 4)
+
+    elif tail == 'M': #P(X = r)
+       if r > 0: #P(X = r), r > 0
+          return round_xsf((nCr(n,r) * (p ** r) * (1 - p) ** (n - r)), 4)
+       
+       else: #P(X = 0)
+          return round_xsf(((1 - p) ** n), 4)
+
+    else: #P(X >= r)
+       if r > 0: #P(X >= r), r > 0
+          return round_xsf((1 - bin_P('L', (r - 1), n, p)), 4)
+       
+       else: #P(X >= 0)
+          return 1
